288f7975a6c9a81c210e988a5bc28cf105210473
Rungrot <rungrot.muangchalerm@gmail.com>
2025-09-08 21:34:48 +0700

    install nodemon

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = 80
 app.use(express.static('public'))
+app.use(express.json())
 app.post('/api/login', (req, res) => {
-    res.status(200).json({ massenger: 'success' })
+    if (!req.body) {
+        res.status(500).json({ massenger: 'error' })
+    } else {
+        res.status(200).json({ massenger: 'success' })
+    }
 })
 app.listen(port, () => {
     console.log(`app listen http://localhost:${(port)}`)
