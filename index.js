0fcca6c82271b5c11a66a876cd9b48ceb6934d47
nithipat22 <wannee857@gmail.com>
2025-09-08 23:25:21 +0700

    api.loggir

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const app = express()
 const port = 80
 app.use(express.static('public'))
 app.use(express.json())
+const users = [
+    { username: 'username', password: 'password', role: 'admin' },
+    { username: 'user', password: 'abcd', role: 'user' }
+];
 app.post('/api/login', (req, res) => {
+    // {
+    // "username","password"
+    // }
     if (!req.body) {
-        res.status(500).json({ massenger: 'error' })
-    } else {
-        res.status(200).json({ massenger: 'success' })
+        res.status(500).json({ message: 'ไม่พบ data' })
     }
+    const { username, password } = req.body;
+    if (!username || !password) {
+        res.status(500).json({ massenger: 'กรุณากรอก uesnam และ password' })
+    }
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (!user) {
+        res.status(401).json({ massenger: 'usename หรือ passeword ไม่ถูกต้อง' });
+    }
+    res.status(200).json({ massenger: 'สำเร็จ' })
+
 })
 app.listen(port, () => {
     console.log(`app listen http://localhost:${(port)}`)
