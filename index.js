7de4a0274a8e31dc308b5c83cd3ea5f60e4dc7b9
nithipat22 <wannee857@gmail.com>
2025-09-08 22:36:34 +0700

    api/login

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 80
 app.use(express.static('public'))
+app.use(express.json())
+
 app.post('/api/login', (req, res) => {
-    res.status(200).json({ massenger: 'success' })
+    if (!req.body) {
+        res.status(500).json({ massenger: 'error' })
+
+    } else {
+        res.status(200).json({ massenger: 'sucess' })
+    }
 })
 app.listen(port, () => {
     console.log(`app listen http://localhost:${(port)}`)
